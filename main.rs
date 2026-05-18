50120ea8cac9ee4d2e637bc8a438dfab3e0a005a
darymyrss1 <slisarenkodora@gmail.com>
2026-05-18 14:11:46 +0300

    main.rs

--- main.rs
+++ main.rs
@@ -1,17 +1,26 @@
+use serde::{Serialize, Deserialize};
+use std::fs;
 use std::env;
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Task {
     id: u32,
     text: String,
+    completed: bool,
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 2 && args[1] == "add" {
-        let task_text = &args[2];
-        println!("План: Добавить задачу '{}' в список", task_text);
-    } else {
-        println!("Использование: add <текст_задачи>");
+        let new_task = Task {
+            id: 1, 
+            text: args[2].clone(),
+            completed: false,
+        };
+        
+        let json = serde_json::to_string(&vec![new_task]).unwrap();
+        fs::write("tasks.json", json).expect("Ошибка записи");
+        println!("Задача сохранена в tasks.json");
     }
 }
